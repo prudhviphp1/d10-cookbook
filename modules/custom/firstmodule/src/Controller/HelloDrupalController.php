130cff3856b8ba4346f7fa03d51e77aff2379ee0
Prudhvi Y <prudhvi.php1@gmail.com>
2023-04-17 12:47:01 +0530

    OP-10: Adding routes & controller.

---
+++ modules/custom/firstmodule/src/Controller/HelloDrupalController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\firstmodule\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class HelloDrupalController extends ControllerBase {
+
+  /**
+   * Returns markup for our custom page.
+   *
+   * @returns array
+   *   The render array.
+   * Defining Controller for the routing
+   */
+  public function page(): array {
+    return [
+      '#markup' => '<p>Hello Drupal!</p>'
+    ];
+  }
+
+}
