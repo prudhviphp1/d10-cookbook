41790c14f6133f0e687c317cd96937f16a7f816b
Prudhvi Y <prudhvi.php1@gmail.com>
2023-04-18 14:04:48 +0530

    OP-15: Adding the path in routing file to create the new articles node.

---
+++ modules/custom/firstmodule/src/Controller/ArticleController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\firstmodule\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Url;
+
+class ArticleController extends ControllerBase {
+
+  public function store(Request $request): JsonResponse {
+
+    $content = $request()->getContent();
+    $json = \Drupal\Component\Serialization\Json::decode($content);
+
+    $entity_type_manager = $this->entityTypeManager();
+    $node_content = $entity_type_manager->getStorage('node');
+
+    $article = $node_content->create([
+      'type'  => 'article',
+      'title' => $json['title'],
+      'body'  => $json['body'],
+      ]);
+    $article->save();
+
+    $article_url = $article->toUrl()->setAbsolute()->toString();
+    return new JsonResponse(
+      $article->toArray(),
+      201,
+      ['Location' => $article_url ],
+    );
+
+  }
+
+}
