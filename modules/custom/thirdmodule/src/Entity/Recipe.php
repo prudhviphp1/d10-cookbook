99a4b96b941a8ce81963b7ee16dc6334c6717d67
Prudhvi Y <prudhvi.php1@gmail.com>
2023-04-20 22:56:12 +0530

    OP-33: Creating the new custom class for entity bundle.

---
+++ modules/custom/thirdmodule/src/Entity/Recipe.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\mymodule\Entity;
+
+use Drupal\node\Entity\Node;
+
+class Recipe extends Node {
+
+  public function getTags(): array {
+    /** @var \Drupal\Core\Field\EntityReferenceFieldItemListInterface $field_tags */
+    // Grabbing the taxonomy terms
+    $field_tags = $this->get('field_tags');
+    return $field_tags->referencedEntities();
+  }
+
+  public function getPrepTime(): int {
+    return (int) $this->get('field_preparation_time')->value;
+  }
+
+  public function getCookTime(): int {
+    return (int) $this->get('field_cooking_time')->value;
+  }
+
+}
