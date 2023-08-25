3ce52fa50c2088256f2fc8dd18e5fadc70aba8eb
prudhvi <prudhvi.php1@gmail.com>
2023-08-25 19:10:54 +0530

    OP-44: Adding the param feature

---
+++ modules/param/src/Controller/WelcomePage.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\custom_general\Controller;
+
+use Drupal\user\UserInterface;
+
+/**
+ * Welcome message print.
+ */
+
+class WelcomePage {
+    /**
+     * Welcome message.
+     */
+    public function welcomePage(UserInterface $user)
+    {
+       $username =  $user->getAccountName();
+       //$username =  $user->label();
+       return [
+        '#markup' => 'Hello ' . $username .  ', Welcome to our custom website!!',
+       ];
+    }
+}
