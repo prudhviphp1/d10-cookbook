cc5142483d3234eac473a670ff45553159b00f4c
prudhvi <prudhvi.php1@gmail.com>
2023-08-26 22:38:54 +0530

    OP-46: Adding the comments

---
+++ modules/custom/custom_config_form/src/Form/CustomConfigForm.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\custom_config_form\Form;
+
+use Drupal\Core\Form\CustomConfigForm;
+use Drupal\Core\Form\FormStateInterface;
+
+class CustomConfigForm extends CustomConfigForm {
+
+    /**
+     * Settings Variables
+     */
+     CONST CONFIGNAME = "custom_config_form.settings";
+
+     /**
+      * {@inheritdoc}
+      * Defining the custom_form_id name for this form
+      */
+      public function getFormId() {
+       return "custom_config_form_settings";
+      }
+
+      /**
+       * {@inheritdoc}
+       */
+       public function getEditableConfigNames() {
+         return [
+           staticname::CONFIGNAME,
+         ];
+       }
+
+       /**
+        * {@inheritdoc}
+        * Defining the buildForm() method
+        * To grab the values from 'custom_config_form_settings.yml' and setting them
+        * as the default values and then giving the flexibility of updating the
+        * values in the fields
+        */
+        public function buildForm(array $form, FormStateInterface $form_state) {
+          $configform = $this->config(static::CONFIGNAME);
+          $form['firstname'] = [
+            '#type' => 'textfield',
+            '#title' => 'First Name',
+            '#default_value' => $configform->getValue('firstname');
+          ],
+          $form['lastname'] = [
+             '#type' => 'textfield',
+             '#title' => 'Last Name',
+             '#default_value' => $configform->getValue('lastname');
+          ],
+          return parent::buildForm($$form,$form_state);
+        }
+
+        /**
+         * {@inheritdoc}
+         * Defining the submitForm method
+         * To grab the values from the buildForm() $form_state and setting them
+         * to the following fields while submitting the form
+         */
+        public function submitForm(array &$form, FormStateInterface $form_state) {
+
+         $configform = $this->config(static::CONFIGNAME);
+         $configform->set('firstname', $this->config->getValue('firstname');
+         $configform->set('lastname', $this->config->getValue('lastname');
+         $configform->save();
+
+         /**
+          * We can also use the following code instead of using the above code
+         $configform = $this->config(static::CONFIGNAME);
+                 ->set('firstname', $this->config->getValue('firstname');
+                 ->set('lastname', $this->config->getValue('lastname');
+                 ->save();
+         */
+        }
+}
